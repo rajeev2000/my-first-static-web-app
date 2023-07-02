6b2925b7ec3db9fa45fb5b0571056791b0f3eb56
Rajeev <rv9850300@gmail.com>
2023-07-02 16:00:22 +0530

    name changed from tuber to IVY

--- src/tuber-news/app.py
+++ src/tuber-news/app.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 while True:
     driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe')#provide the local download location of chrome driver for my case it is in downloads
-    driver.get('https://www.tuber.org.in/')
+    driver.get('https://www.Ivyaffliate.online/')
     driver.maximize_window()
     sleep(3)
     driver.execute_script("window.scrollBy(0,2000)","")
